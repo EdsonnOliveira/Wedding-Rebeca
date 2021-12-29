fcd84436e1bdaaffd81d795b4867b99bcc83497f
Edson Pinheiro de Oliveira <edsonpinheirodeoliveira@Edsons-MacBook-Pro.local>
2021-12-29 09:02:39 -0300

    Adjusted Count Days

--- JS/countDays.js
+++ JS/countDays.js
@@ -1,24 +1,22 @@
 var target_date = new Date("april 16, 2022").getTime();
-var month, day, hour, minute, second;
+
+var days, hours, minutes, seconds;
 
 setInterval(function () {
     var current_date = new Date().getTime();
-    var second_f = (target_date - current_date) / 1000;
+    var seconds_f = (target_date - current_date) / 1000;
 
-    month = parseInt(second_f / 86400) / 30;
-
-    day = parseInt(Number(String(month).substr(0, 1)) * 30.417);
-    second_f = second_f % 86400;
+    days = parseInt(seconds_f / 86400);
+    seconds_f = seconds_f % 86400;
     
-    hour = parseInt(second_f / 3600);
-    second_f = second_f % 3600;
+    hours = parseInt(seconds_f / 3600);
+    seconds_f = seconds_f % 3600;
     
-    minute = parseInt(second_f / 60);
-    second = parseInt(second_f % 60);
+    minutes = parseInt(seconds_f / 60);
+    seconds = parseInt(seconds_f % 60);
 
-    // document.getElementById('month').innerHTML = String(month).substr(0, 1);
-    document.getElementById('day').innerHTML = day;
-    document.getElementById('hour').innerHTML = hour;
-    document.getElementById('minute').innerHTML = minute;
-    document.getElementById('second').innerHTML = second;
-}, 1000)
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+}, 1000);
